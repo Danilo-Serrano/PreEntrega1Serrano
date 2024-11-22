9052b80f9b6c56ed57cf4535a6fff31f2d108865
Danilo Serrano <serranodanilo10@yahoo.com.ar>
2024-11-21 22:17:23 -0300

    segunda pre entrega

--- js/main.js
+++ js/main.js
@@ -1,40 +1,71 @@
-//Turnos Hospital//
+//urgente - considerable - leve
 
-//nombre paciente, edad, tipo de consulta//
-let turnos = parseInt(prompt("¿Cuántos turnos quieres ingresar?"))
-for(let i = 0; i < turnos; i++){
-console.log("Turno " + i)
-let nombre =prompt("Ingresa nombre del paciente: ")
-if (nombre == ""){
-    console.log("Nombre Incorrecto")
-    break;
+alert("Bienvenido a la página web del Hospital Mendoza")
+const opcion = prompt("1- Ver turnos 2- Crear turno 3- Modificar turnos 0- Salir")
+class pacientes{
+    constructor(nombre, edad, especialidad, urgencia ){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.especialidad = especialidad;
+        this.urgencia = urgencia;
+    }
 }
-console.log(nombre);
-let edad =parseInt(prompt("Ingresa edad del paciente: "))
-if(edad >= 18){
-    console.log("Mayor de edad " + "(" + edad + ")")
-}else if( isNaN(edad) || edad == ""){
-    console.log("Edad Incorrecta")
-    break;
-}
-else{
-    console.log("Menor de edad " + "(" + edad + ")")
-}
-let consulta =prompt("Ingresa tipo de consulta del paciente: ")
 
-switch (consulta) {
-    case "urgente":
-        console.log("Tipo de consulta " + consulta)
-        console.log("Tendrá Prioridad en los turnos " + nombre)
+switch (opcion) {
+    case "1":
+        const paciente1 = new pacientes("Aldo", 77, "traumatología", "leve") 
+        const paciente2 = new pacientes("Carlos", 91, "cardiología","considerable") 
+        const paciente3 = new pacientes("Ticiano", 35, "endoctrinología","urgente") 
+
+        console.log("Pacientes del día de hoy: " + (new Date()))
+        console.log(paciente1)
+        console.log(paciente2)
+        console.log(paciente3)
         break;
+
+    case "2":
+            function crearPaciente(){
+                const nombre = prompt("Introduce Nombre del paciente")
+                const edad = prompt("Introduce edad del paciente")
+                const especialidad = prompt("Introduce especialidad del paciente")
+                const urgencia = prompt("Introduce urgencia del paciente")
     
-    case "regular":
-    case "control":
-        console.log("Tipo de consulta " + consulta)
+                return new pacientes(nombre, edad, especialidad, urgencia);
+            }
+            const paciente4 = crearPaciente()
+    
+            console.log(paciente4)
+            break;
+    case "3":
+        alert("Historial de turnos pasados")
+        const turnosPasados =[
+            {
+                nombre: "Pepe",
+                edad: 47,
+                urgencia: "urgente"
+            },{
+                nombre: "zandro",
+                edad: 21,
+                urgencia: "leve"
+            },{
+                nombre: "Armando",
+                edad: 86,
+                urgencia: "considerable"
+            }
+        ]
+        console.log(turnosPasados)
+        const opcion2 = prompt("¿Quieres ordenarlos alfabeticamente?")
+        if (opcion2.toLowerCase() === "si") {
+            // Crear un nuevo array con los turnos ordenados alfabéticamente
+            const turnosOrdenados = [...turnosPasados].sort((a, b) => a.nombre.localeCompare(b.nombre));
+    
+            console.log("Turnos pasados ordenados alfabéticamente:");
+            console.log(turnosOrdenados);
+        }
         break;
 
     default:
-        console.log("No existe ese tipo de consulta")
+        alert("Saliste del programa")
         break;
 }
-}
+//herencia, para modificar
