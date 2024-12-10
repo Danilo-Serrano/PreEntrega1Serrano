37b31baaffbef99cd5f2e4c8cad1a7f269c1b6f6
Danilo Serrano <serranodanilo10@yahoo.com.ar>
2024-12-10 16:44:33 -0300

    tercera entrega

--- js/main.js
+++ js/main.js
@@ -1,160 +1,132 @@
-//Inicio del Programa
-const pacientes = []
+//Funciones
+function renderizarTurno(){
+    const tableBody = document.getElementById("tableBody")
+    tableBody.innerHTML = "";
+    for(const turno of turnos){
+        tableBody.innerHTML = tableBody.innerHTML + `<tr>
+        <td class="style--cells2">${turno.nombre}</td>
+        <td class="style--cells2">${turno.edad}</td>
+        <td class="style--cells2">${turno.urgencia}</td>
+    </tr>`
+    }
+}
 
-let opcion;
-do {
-    opcion =parseInt(prompt("1- Crear Turno, 2- Ver todos los turnos, 3- Eliminar turnos, 4- Ordenar turnos 0- Salir") )
-    if(opcion == 1){
-        pacientes.push(
+function obtenerDeLs() {
+    const turnosJSON = localStorage.getItem("turnos"); // Obtener datos de localStorage
+    if (turnosJSON) {
+        return JSON.parse(turnosJSON); // Si existen, retorna el array parseado
+    }
+    return []; // Si no hay datos, retorna un array vacío
+}
+
+function guardarenLS() {
+    localStorage.setItem("turnos", JSON.stringify(turnos)); // Guarda el array en formato JSON
+}
+
+
+function regristrarTurnos(){
+    const formulario = document.getElementById("form")
+    const nombre = document.getElementById("input__nombre")
+    const edad = document.getElementById("input__edad")
+    const urgencia = document.getElementById("input__urgencia")
+
+    formulario.addEventListener("submit", (e) =>{
+        e.preventDefault()
+
+        turnos.push(
             {
-                nombre: prompt("Ingresa nombre del paciente"),
-                apellido: prompt("Ingresa apellido del paciente"),
-                edad: parseInt(prompt("Ingresa edad del paciente")),
-                urgencia: prompt("Ingresa urgencia del paciente"),
+                    nombre: nombre.value,
+                    edad: edad.value,
+                    urgencia: urgencia.value,
             }
-            
         )
-        const bodyT = document.getElementById("bodyTabla")
-        bodyT.innerHTML = "";
+        console.log("turno guardado")
+        console.log(turnos)
+        nombre.value = ""
+        edad.value =""
+        urgencia.value = ""
 
-        function ordenarTablaAlfabetico(){
-            for(const paciente of pacientes){
-                bodyT.innerHTML = bodyT.innerHTML + "<tr>" +
-                "<td>"+paciente.nombre +"</td>" +
-                "<td>"+paciente.apellido +"</td>" +
-                "<td>"+paciente.edad +"</td>" +
-                "<td>"+paciente.urgencia +"</td>" +
-            "</tr>";
+        renderizarTurno()
+
+        ordenarAlfa()
+
+        ordenarEdad()
+
+        guardarenLS()
+    })
+}
+function eliminarTurnos(){
+    const form2 = document.getElementById("form2")
+    const input__eliminar = document.getElementById("input__eliminar")
+
+    form2.addEventListener("submit", (e)=>{
+        e.preventDefault()
+        for(let i = 0; i < turnos.length; i++){
+            if(input__eliminar.value.toLowerCase() == turnos[i].nombre.toLowerCase() ){
+                turnos.splice(i ,1)
+                console.log("turno eliminado")
+                console.log(turnos)
+                input__eliminar.value = ""
+                renderizarTurno()
+                guardarenLS()
             }
+        }
+    })
+    }
+    function ordenarAlfa(){
+        const alfabeto = document.getElementById("button--alfabeticamente")
+        alfabeto.addEventListener("click", () =>{
+            
+            turnos.sort( (a, b) =>{
+            if(a.nombre.toLowerCase() > b.nombre.toLowerCase()){
+                return 1;
+            }else if(a.nombre.toLowerCase() < b.nombre.toLowerCase()){
+                return -1;
             }
-            ordenarTablaAlfabetico()
-
-        console.log("Turno guardado")
-        
+            return 0;
+            })
+            console.log("Ordenado Alfabeticamente")
+            console.log(turnos)
+            renderizarTurno()
+            guardarenLS()
+        })
     }
-    let copiaPacientes = pacientes.slice(0, pacientes.length)
-    if(opcion == 2){
-        console.log("Lista de Pacientes")
-        console.log(copiaPacientes)
-        
-    }
-
-    if(opcion == 3){
-        const pacienteAborrar = prompt("¿Qué turno quiere eliminar?, (Escriba el nombre del paciente)")
-        for(i = 0; i < pacientes.length; i++){
-
-            if(pacientes[i].nombre == pacienteAborrar){
-                const final = pacientes.splice(i ,1)
-                console.log("Turno Borrado")
+function ordenarEdad(){
+    const buttonEdad = document.getElementById("button--porEdad")
+    buttonEdad.addEventListener("click", () =>{
+        turnos.sort( (a,b) =>{
+            if(a.edad > b.edad){
+                return 1;
+            }else if(a.edad < b.edad){
+                return -1;
             }else{
-                console.log("Turno No encontrado")
+                return 0;
             }
-        }
-    }
-    if(opcion == 4){
-        const ordenar = prompt("¿Cómo quieres ordenar los turnos? (Alfabeticamente, edad, urgencia)")
-        if(ordenar.toLowerCase() == "alfabeticamente"){
-            let segundaCopiaPacientes = pacientes.slice(0, pacientes.length)
+        })
+        console.log("Ordenando Por edad")
+        console.log(turnos)
+        renderizarTurno()
+        guardarenLS()
+    } )
 
-            segundaCopiaPacientes.sort((a ,b) =>{
-                if(a.nombre.toLowerCase() > b.nombre.toLowerCase()){
-                    return 1;
-                }else if(b.nombre.toLowerCase() > a.nombre.toLowerCase()){
-                    return -1;
-                }else{
-                    return 0;
-                }
-            })
-            console.log("Turnos ordenados alfabeticamente")
-            console.log(segundaCopiaPacientes)
-
-            const ordenar = document.getElementById("bodyTabla")
-            ordenar.innerHTML = "";
-
-            function ordenarTablaAlfabetico(){
-                const ordenador = document.getElementById("ordenando")
-                ordenador.innerText = ordenador.innerHTML + "Alfabeticamente"
-
-                for(const pacientes1 of segundaCopiaPacientes){
-                ordenar.innerHTML = ordenar.innerHTML + "<tr>" +
-                    "<td>"+ pacientes1.nombre +"</td>" +
-                    "<td>"+ pacientes1.apellido +"</td>" +
-                    "<td>"+ pacientes1.edad +"</td>" +
-                    "<td>"+ pacientes1.urgencia +"</td>" +
-                "</tr>";
-                }
-                }
-
-                ordenarTablaAlfabetico()
-        }
-        if(ordenar.toLowerCase() == "edad"){
-            let terceraCopiaPacientes = pacientes.slice(0, pacientes.length)
-
-            terceraCopiaPacientes.sort((a, b) =>{
-                if(a.edad > b.edad){
-                    return -1;
-                }else if(b.edad > a.edad){
-                    return 1;
-                }else{
-                    return 0;
-                }
-            })
+}
+// //Inicio Programa
+let turnos = obtenerDeLs()
+let opcion;
 
 
-            function ordenarTablaEdad(){
-                const ordenador = document.getElementById("ordenando")
-                ordenador.innerText = ordenador.innerHTML + "Por edad"
-                ordenador.append()
+    renderizarTurno()
 
-                const ordenE = document.getElementById("bodyTabla")
-                ordenE.innerHTML = ""
-                for(const pacientes2 of terceraCopiaPacientes){
-                    ordenE.innerHTML = ordenE.innerHTML + "<tr>" +
-                    "<td>"+ pacientes2.nombre +"</td>" +
-                    "<td>"+ pacientes2.apellido +"</td>" +
-                    "<td>"+ pacientes2.edad +"</td>" +
-                    "<td>"+ pacientes2.urgencia +"</td>" +
-                "</tr>";
-                }
-            }
-            ordenarTablaEdad()
+    regristrarTurnos()
 
-            console.log("Turnos ordenados por edad (Mayor a Menor)")
-            console.log(terceraCopiaPacientes)
-        }
-        //Arreglar poder escribir urgente en minus o mayus
-        if(ordenar.toLowerCase() == "urgencia"){
-            let cuartaCopiaPacientes = pacientes.slice(0, pacientes.length)
-            cuartaCopiaPacientes.sort((a,b) =>{
-                if(a.urgencia === "urgente" && b.urgencia !== "urgente"){
-                    return -1;
-                }else if (a.urgencia !== "urgente" && b.urgencia === "urgente"){
-                    return 1;
-                }else{
-                    return 0;
-                }
-            });
-            function ordenarTablaUrgencia(){
-                const ordenador = document.getElementById("ordenando")
-                ordenador.innerText = ordenador.innerHTML + "Por Urgencia"
-                ordenador.append()
+    eliminarTurnos()
 
-                const ordenU = document.getElementById("bodyTabla")
-                ordenU.innerHTML = ""
-                for(const pacientes3 of cuartaCopiaPacientes){
-                    ordenU.innerHTML = ordenU.innerHTML + "<tr>" +
-                    "<td>"+ pacientes3.nombre +"</td>" +
-                    "<td>"+ pacientes3.apellido +"</td>" +
-                    "<td>"+ pacientes3.edad +"</td>" +
-                    "<td>"+ pacientes3.urgencia +"</td>" +
-                "</tr>";
-                }
-            }
-            ordenarTablaUrgencia()
 
-            console.log("Pacientes con priodidad de urgencia", cuartaCopiaPacientes)
+    
 
-        }
-    }
-} while ((opcion !== 0 ))
-console.log("Saliendo del Programa...");
+console.log("Saliendo del Programa...")
+
+
+
+
+
